e31159040fd3cf89c0b53fe835b27bc781342b02
Melvin-Elenneny <279225407+Melvin-Elenneny@users.noreply.github.com>
2026-04-25 19:35:49 +0200

    test modification

--- classe_inverse.py
+++ classe_inverse.py
@@ -1,4 +1,42 @@
-print("test")
+''' Projet Jeu du pendu, classe inversé, groupe C
 
-# Test de sauvegarde
+IMPORTANT ! MERCI DE BIEN LIRE !
+
+Instructions de sauvegardes si, et seulement si vous avez été ajouté en tant que COLLABORATEUR :
+Respectez bien les majuscules, espace, autres caractères...
+
+Etape 1 (entrée dans nouveau terminal): Allez dans "Terminal" en haut à gauche / New Terminal / dans le terminal qui s'affichera en bas, tapez : cd Desktop
+
+Etape 2 (cloner) : toujours dans le terminal, tapez : git clone https://github.com/Melvin-Elenneny/git-grpc.git
+
+Etape 3 (entrée dans le dossier) : toujours dans le terminal, tapez : cd Classe_inverse_groupeC
+
+Etape 4 (l'ouverture du dossier dans votre logiciel VSC) : code .
+ATTENTION ! il y a un espace entre "code" et "."
+
+Etape 5 (récupération des dernières modifications du programme): toujours dans le terminal, tapez : git pull
+
+A partir de là, vous pouvez travailler normalement sur le projet. Une fois finis, sauvegardez vos modifications :
+
+Etape 6 (sauvegarder les modifications) : dans le terminal, tapez : git add .
+Il y a encore un espace entre "add" et "."
+
+Etape 7 (création d'une sauvegarde) : dans le terminal, tapez : git commit -m "description des modifications"
+Faites le mot pour mot !
+
+Etape 8 (envoyer sur GitHub) : dans le terminal, tapez : git push
+
+
+Merci de prendre en compte toutes les étapes minutieusement, et ne pas modifier / effacer le texte ci-dessus sans m'en informer.
+
+Pour tout problème, merci de me contacter en MP sur Sonate (Melvin Lannoy), ou de passer par le forum de discussion Introduction à la programmation, ou de contacter votre tuteur.
+
+Bon travail à vous, groupe C !!
+
+'''
+
+
+
+
+
 
